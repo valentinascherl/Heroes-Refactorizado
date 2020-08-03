98b7185e5d2ab8c66a3bfcfa184f4bb30473a708
Valentina Scherl <valescherl@yahoo.com.ar>
2020-08-03 11:05:21 -0300

    refactorizado

--- controllers/heroesController.js
+++ controllers/heroesController.js
@@ -1,7 +1,7 @@
 // Require de FS
 const fs = require('fs');
 // Leyendo y parseando (en array) el contenido de heroes.json
-const heroes = JSON.parse(fs.readFileSync(__dirname + '/data/heroes.json', 'utf-8'));
+const heroes = JSON.parse(fs.readFileSync('./data/heroes.json', 'utf-8'));
 
 
 let heroesController = {
--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,7 +1,8 @@
 let mainController = {
     home: function(req,res){
     res.send("Ni Superman, Iron Man o La Mujer Maravilla son tan importantes cómo las y los Heroes de carne y hueso que encontrarás en este sitio. Esperamos que ellas y ellos te sirvan como    ti!");
-}, creditos: function(req,res){
+}, 
+   creditos: function(req,res){
     res.send("Credito de las y los developers.");
 }
 }
